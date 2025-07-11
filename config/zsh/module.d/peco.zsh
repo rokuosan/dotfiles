ad44fc76c4fefc480d3b256f569adaa051028c4c
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-07-11 21:48:10 +0900

    zshrc: add ghq integration for directory selection with peco

--- config/zsh/module.d/peco.zsh
+++ config/zsh/module.d/peco.zsh
@@ -7,4 +7,22 @@ if (( $+commands[peco] )); then
 
     zle -N peco-history-selection
     bindkey '^R' peco-history-selection
+
+    if (( $+commands[ghq] )); then
+        function peco-ghq-look () {
+            local ghq_roots="$(git config --path --get-all ghq.root)"
+            local selected_dir=$(ghq list --full-path | \
+                xargs -I{} gls -dl --time-style=+%s {}/.git | sed 's/.*\([0-9]\{10\}\)/\1/' | sort -nr | \
+                sed "s,.*\(${ghq_roots/$'\n'/\|}\)/,," | \
+                sed 's/\/.git//' | \
+                peco --prompt="cd-ghq >" --query "$LBUFFER")
+            if [ -n "$selected_dir" ]; then
+                BUFFER="cd $(ghq list --full-path | grep --color=never -E "/$selected_dir$")"
+                zle accept-line
+            fi
+        }
+
+        zle -N peco-ghq-look
+        bindkey '^G' peco-ghq-look
+    fi
 fi

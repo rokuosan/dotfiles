e7a468079c4518342f9947c2825a79940203966e
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-07-11 22:28:42 +0900

    zshrc: simplify peco-ssh function and reorganize ghq integration

--- config/zsh/module.d/peco.zsh
+++ config/zsh/module.d/peco.zsh
@@ -10,15 +10,7 @@ if (( $+commands[peco] )); then
 
     # SSH
     function peco-ssh () {
-        local selected_host=$(awk '
-        tolower($1)=="host" {
-            for (i=2; i<=NF; i++) {
-                if ($i !~ "[*?]") {
-                    print $i
-                }
-            }
-        }
-        ' ~/.ssh/config | sort | peco --query "$LBUFFER")
+        local selected_host=$(grep -E "^Host " ~/.ssh/config | awk '{print $2}' | grep -v '*' | peco --query "$LBUFFER")
         if [ -n "$selected_host" ]; then
             BUFFER="ssh ${selected_host}"
             zle accept-line
@@ -26,24 +18,34 @@ if (( $+commands[peco] )); then
         zle clear-screen
     }
     zle -N peco-ssh
-    bindkey '^\' peco-ssh
+    bindkey '^_' peco-ssh
 
     # ghq
-    if (( $+commands[ghq] )); then
-        function peco-ghq-look () {
-            local ghq_roots="$(git config --path --get-all ghq.root)"
-            local selected_dir=$(ghq list --full-path | \
-                xargs -I{} gls -dl --time-style=+%s {}/.git | sed 's/.*\([0-9]\{10\}\)/\1/' | sort -nr | \
-                sed "s,.*\(${ghq_roots/$'\n'/\|}\)/,," | \
-                sed 's/\/.git//' | \
-                peco --prompt="cd-ghq >" --query "$LBUFFER")
-            if [ -n "$selected_dir" ]; then
-                BUFFER="cd $(ghq list --full-path | grep --color=never -E "/$selected_dir$")"
-                zle accept-line
-            fi
-        }
+    function peco-ghq-look () {
+        local ghq_roots="$(git config --path --get-all ghq.root)"
+        local selected_dir=$(ghq list --full-path | \
+            xargs -I{} gls -dl --time-style=+%s {}/.git | sed 's/.*\([0-9]\{10\}\)/\1/' | sort -nr | \
+            sed "s,.*\(${ghq_roots/$'\n'/\|}\)/,," | \
+            sed 's/\/.git//' | \
+            peco --prompt="cd-ghq >" --query "$LBUFFER")
+        if [ -n "$selected_dir" ]; then
+            BUFFER="cd $(ghq list --full-path | grep --color=never -E "/$selected_dir$")"
+            zle accept-line
+        fi
+    }
+    zle -N peco-ghq-look
+    bindkey '^G' peco-ghq-look
 
-        zle -N peco-ghq-look
-        bindkey '^G' peco-ghq-look
-    fi
+    function peco-action-menu() {
+        local -A actions=(
+            ["Go to Repository(^G)"]="peco-ghq-look"
+            ["SSH Connect(^_)"]="peco-ssh"
+            ["History Search(^R)"]="peco-history-selection"
+        )
+
+        local selected=$(printf '%s\n' "${(@k)actions}" | peco --prompt="Action: ") || return
+        eval "${actions[$selected]}"
+    }
+    zle -N peco-action-menu
+    bindkey '^\' peco-action-menu
 fi

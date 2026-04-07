d9bcd3b96cfc8c0a461accb24a5178a92a7b3789
Koki Matsumoto <git@rokuosan.dev>
2026-04-08 01:13:18 +0900

    キャッシュを入れる

--- config/zsh/module.d/completion.zsh
+++ config/zsh/module.d/completion.zsh
@@ -1,5 +1,28 @@
 autoload -Uz compinit
-compinit -i
+
+zstyle ':completion:*' use-cache on
+typeset -g zsh_cache_base="${XDG_CACHE_HOME:-$HOME/.cache}"
+if [[ ! -d $zsh_cache_base && ! -w ${zsh_cache_base:h} ]]; then
+    zsh_cache_base="${TMPDIR:-/tmp}"
+fi
+
+typeset -g zsh_cache_dir="$zsh_cache_base/zsh"
+typeset -g zcompdump_path="$zsh_cache_dir/.zcompdump"
+
+[[ -d $zsh_cache_dir ]] || mkdir -p "$zsh_cache_dir" 2>/dev/null
+if [[ ! -d $zsh_cache_dir ]]; then
+    zsh_cache_dir="${TMPDIR:-/tmp}/zsh-$UID"
+    zcompdump_path="$zsh_cache_dir/.zcompdump"
+    [[ -d $zsh_cache_dir ]] || mkdir -p "$zsh_cache_dir" 2>/dev/null
+fi
+
+zstyle ':completion:*' cache-path "$zsh_cache_dir/completion"
+
+if [[ -s $zcompdump_path(#qN.mh+24) ]]; then
+    compinit -i -d "$zcompdump_path"
+else
+    compinit -C -i -d "$zcompdump_path"
+fi
 
 # Shell Options
 setopt ALWAYS_TO_END
@@ -27,4 +50,6 @@ zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # Suggest
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+if [[ -r /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+    source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi

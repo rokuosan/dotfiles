6a2f33b35bd5dc79b366b2dc99a6657b1b94f0f2
Koki Matsumoto <git@rokuosan.dev>
2026-07-07 13:50:48 +0900

    fmt

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 wezterm.on("bell", function(window, pane)
-  local title = pane:get_title() or "WezTerm"
-  local message = "Terminal bell at " .. wezterm.strftime("%H:%M:%S")
-  window:toast_notification(title, message, nil, 4000)
+	local title = pane:get_title() or "WezTerm"
+	local message = "Terminal bell at " .. wezterm.strftime("%H:%M:%S")
+	window:toast_notification(title, message, nil, 4000)
 end)
 
 local config = wezterm.config_builder()
@@ -16,9 +16,9 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 16
 config.harfbuzz_features = {
-  "calt=0",
-  "dlig=0",
-  "liga=0",
+	"calt=0",
+	"dlig=0",
+	"liga=0",
 }
 
 config.color_scheme = "Tokyo Night Moon"
@@ -27,10 +27,10 @@ config.window_background_opacity = 1.0
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.initial_cols = 160
@@ -42,41 +42,41 @@ config.term = "xterm-256color"
 config.quit_when_all_windows_are_closed = true
 
 config.keys = {
-  {
-    key = "Tab",
-    mods = "CTRL",
-    action = act.SendString("]b"),
-  },
-  {
-    key = "Tab",
-    mods = "CTRL|SHIFT",
-    action = act.SendString("[b"),
-  },
-  {
-    key = "d",
-    mods = "CMD",
-    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "d",
-    mods = "CMD|SHIFT",
-    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "]",
-    mods = "CMD",
-    action = act.ActivatePaneDirection("Next"),
-  },
-  {
-    key = "[",
-    mods = "CMD",
-    action = act.ActivatePaneDirection("Prev"),
-  },
-  {
-    key = "Enter",
-    mods = "SHIFT",
-    action = act.SendString("\n"),
-  },
+	{
+		key = "Tab",
+		mods = "CTRL",
+		action = act.SendString("]b"),
+	},
+	{
+		key = "Tab",
+		mods = "CTRL|SHIFT",
+		action = act.SendString("[b"),
+	},
+	{
+		key = "d",
+		mods = "CMD",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "]",
+		mods = "CMD",
+		action = act.ActivatePaneDirection("Next"),
+	},
+	{
+		key = "[",
+		mods = "CMD",
+		action = act.ActivatePaneDirection("Prev"),
+	},
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = act.SendString("\n"),
+	},
 }
 
 return config
